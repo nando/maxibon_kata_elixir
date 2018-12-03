171e73895f72bccd61a3d97c9f9201b56472ae58
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2018-12-03 12:46:57 +0100

    set office name :min_length in generation helper

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,8 @@ require ExUnitProperties
 
 defmodule TestHelper do
   def karumi_hq_generator do
-    ExUnitProperties.gen all name <- StreamData.string(:alphanumeric) do
+    ExUnitProperties.gen all name <- StreamData.string( :alphanumeric,
+                                                        min_length: 8 ) do
                            MaxibonKataElixir.karumi_hq( name )
                          end
   end
