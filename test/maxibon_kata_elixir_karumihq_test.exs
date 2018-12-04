a56b799915ca9826d4b355e8f1df7407ea628901
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2018-12-04 10:44:16 +0100

    move generators to test_helper

--- test/maxibon_kata_elixir_karumihq_test.exs
+++ test/maxibon_kata_elixir_karumihq_test.exs
@@ -1,16 +1,11 @@
+require TestHelper
+
 defmodule MaxibonKataElixirKarumiHQTest do
   use ExUnit.Case, async: true
   use ExUnitProperties
 
-  def karumi_hq_generator do
-    ExUnitProperties.gen all name <- string( :alphanumeric,
-                                             min_length: 8 ) do
-                           MaxibonKataElixir.karumi_hq( name )
-                         end
-  end
-
   property "should start the day with 10 maxibons" do
-    check all office <- karumi_hq_generator() do
+    check all office <- TestHelper.karumi_hq_generator() do
       assert office[ :maxibons_left ] == 10
     end
   end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,20 @@
+defmodule TestHelper do
+  use ExUnitProperties
+
+  def karumi_hq_generator do
+    gen all name <- string( :alphanumeric,
+                            min_length: 8 ) do
+          MaxibonKataElixir.karumi_hq( name )
+        end
+  end
+
+  def unpredictable_developer_generator do
+    gen all name <- string( :alphanumeric,
+                            min_length: 3 ),
+            maxibons_to_grab <- integer() do
+          MaxibonKataElixir.developer( name, maxibons_to_grab )
+        end
+  end
+end
+
 ExUnit.start()
