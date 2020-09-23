9b80937cd27dd321dba7c4296e721d06e4e813d4
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2020-09-23 15:59:38 +0200

    always > 2 maxibons even if grab in group property GREEN!!

--- lib/maxibon_kata_elixir.ex
+++ lib/maxibon_kata_elixir.ex
@@ -1,5 +1,11 @@
 defmodule MaxibonKataElixir do
-  def open_fridge( office, developer ) do
-    MaxibonKataElixir.Developer.grab_maxibons developer, office
+  def open_fridge( office, dev_or_devs ) do
+    if is_list(dev_or_devs) do
+      Enum.reduce(dev_or_devs,
+                  office,
+                  fn(dev, final_office) -> MaxibonKataElixir.Developer.grab_maxibons(dev, final_office) end)
+    else
+      MaxibonKataElixir.Developer.grab_maxibons dev_or_devs, office
+    end
   end
 end
